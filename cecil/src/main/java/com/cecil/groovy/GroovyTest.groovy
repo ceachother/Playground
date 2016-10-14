47357b32dbf0e8652eb84d5045f7248fc189212e
Cecil Lee <mingchi.li@djhealthunion.com.cn>
2016-10-14 18:11:01 +0800

    add groovy

--- cecil/src/main/java/com/cecil/groovy/GroovyTest.groovy
+++ cecil/src/main/java/com/cecil/groovy/GroovyTest.groovy
@@ -22,4 +22,33 @@ class GroovyTest extends GroovyTestCase {
         aClosure "1",2
     }
 
+    @Test
+    void File() {
+        def File targetFile = new File("build.gradle")
+        targetFile.eachLine {
+            String line ->
+                println line
+        }
+    }
+
+    @Test
+    void inp() {
+        def File targetFile = new File("build.gradle")
+        def ism =  targetFile.newInputStream()
+        //操作ism，最后记得关掉
+        ism.close
+    }
+
+    @Test
+    void outp() {
+        def srcFile = new File("")
+        def targetFile = new File("")
+        targetFile.withOutputStream{
+            os-> srcFile.withInputStream {
+                ins->
+                    os << ins //利用OutputStream的<<操作符重载，完成从inputstream到OutputStream  //的输出
+            }
+        }
+    }
+
 }
