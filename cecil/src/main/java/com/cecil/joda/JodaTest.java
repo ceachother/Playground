af20e52c2e1bafc4c82727aa9e63a65d0280abf4
Cecil Lee <mingchi.li@djhealthunion.com.cn>
2016-10-21 11:32:49 +0800

    add methods in JodaTest.java

--- cecil/src/main/java/com/cecil/joda/JodaTest.java
+++ cecil/src/main/java/com/cecil/joda/JodaTest.java
@@ -8,7 +8,8 @@
 package com.cecil.joda;
 
 import org.joda.time.DateTime;
-import org.joda.time.DateTimeUtils;
+
+import java.util.Date;
 
 /**
  * @author LiMingChi 2016-09-20 17:42
@@ -17,10 +18,31 @@ import org.joda.time.DateTimeUtils;
 public class JodaTest {
 
     public static void main(String[] args) {
-//        DateTimeUtils.setCurrentMillisFixed(1);
+        stringToJoda();
+//        jodaToString();
+    }
+
+    public static void jodaToString() {
         DateTime dateTime = new DateTime();
         System.out.println(dateTime);
-        System.out.println(dateTime.toString("yyyymmdd"));
+        System.out.println(dateTime.toString("yyyyMMdd"));
+        System.out.println(new DateTime(new Date().getTime()).toString("yyyyMMdd"));
+    }
+    public static void stringToJoda() {
+//        DateTimeFormatter dateTimeFormatter = DateTimeFormat.forPattern("yyyyMMdd");
+//        DateTime dateTime = DateTime.parse("20161020",dateTimeFormatter);
+//        System.out.println(dateTime);
+//        dateTime = dateTime.plusDays(-1);
+//        System.out.println(dateTime.toString("yyyyMMdd"));
+//        dateTime = dateTime.withTime(23,59,59,0);
+//        System.out.println(dateTime);
+
+        DateTime end = DateTime.now().minusDays(5);
+        System.out.println(end);
+        String endDate = "1".isEmpty() ? end.toString("yyyyMMdd"):"";
+        System.out.println(endDate);
+        System.out.println(DateTime.now().toString("yyyyMMdd"));
+
     }
 
 }
